4e4d1e5721f3d7b6f0dfa5091611346534b3249d
Inna <prizrak1609@gmail.com>
2023-09-06 22:52:58 +0300

    Добавьте файлы проекта.

---
+++ Point.h
@@ -0,0 +1,13 @@
+#pragma once
+class Point
+{
+	int x;
+	int y;
+public:
+	Point();
+	Point(int a, int b);
+
+	int getPointX();
+	int getPointY();
+};
+std::ostream& operator<<(std::ostream& os, Point& point);
---
+++ ООП Задание 13.1.cpp
@@ -0,0 +1,24 @@
+﻿//Создайте вектор для хранения элементов типа Point. Сохраните в этом векторе 5 элементов типа Point.
+//Выведите элементы сохраненного вектора на экран.
+
+#include <iostream>
+#include<vector>
+#include"Point.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    Point A(5, 10);
+    Point B(13, 6);
+    Point C(8, 8);
+    Point D(0, 0);
+    Point E(1, 0);
+
+    vector<Point> arreyPoint = { A, B, C, D, E };
+
+    for (int i = 0; i < 5; i++) {
+        cout << arreyPoint.at(i) << endl;
+    }
+}
